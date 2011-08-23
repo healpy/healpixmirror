4cc8ebd8ee47bba090ee524f09099e974a7005dc
reinecke <reinecke@5e3cd54f-fed5-4059-89be-b68eacfec290>
2011-08-23 12:45:47 +0000

    simplify step computation
    git-svn-id: svn://svn.code.sf.net/p/healpix/code/trunk@167 5e3cd54f-fed5-4059-89be-b68eacfec290

--- src/java/src/healpix/plot3d/gui/healpix3d/HealSphere.java
+++ src/java/src/healpix/plot3d/gui/healpix3d/HealSphere.java
@@ -36,116 +36,115 @@ import javax.vecmath.Point3d;
  * A base class for different healpix sphere representations (??). Adaptation by
  * R.Carr from earlier code by G.Giardino (& O'Mullane??).
  */
-public abstract class HealSphere extends Shape3D {
+public abstract class HealSphere extends Shape3D
+  {
+  /** The nside. */
+  protected int nside = 2;
 
-	/** The nside. */
-	protected int nside = 2;
+  /**
+    * Number of sections to use when drawing side of a pixel as series of
+    * lines.
+    */
+  protected int step = 60;
 
-	/**
-	 * Number of sections to use when drawing side of a pixel as series of
-	 * lines.
-	 */
-	protected int step = 60;
+  /** Descriptor object for a healpix sphere of a specific nside. */
+  protected HealpixBase index;
 
-	/** Descriptor object for a healpix sphere of a specific nside. */
-	protected HealpixBase index;
+  protected void setStep()
+    {
+    step=10;
+    if (nside >= 8)
+      step = 6;
+    if (nside >= 16)
+      step = 4;
+    if (nside >= 32)
+      step = 2;
+    if (nside >= 64)
+      step = 1;
+    }
 
-	/**
-	 * Constructor for specified nside.
-	 * 
-	 * @param nside
-	 *            the healpix resolution parameter.
-	 */
-	public HealSphere(int nside, Scheme scheme) {
-		this.nside = nside;
-		if (nside >= 8)
-			step = 11;
-		if (nside >= 16)
-			step = 7;
-		if (nside >= 32)
-			step = 3;
-		if (nside >= 64)
-			step = 1;
-		try {
-			index = new HealpixBase(nside,scheme);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+  /**
+    * Constructor for specified nside.
+    *
+    * @param nside
+    *            the healpix resolution parameter.
+    */
+  public HealSphere(int nside, Scheme scheme) {
+    this.nside = nside;
+    setStep();
+    try {
+            index = new HealpixBase(nside,scheme);
+    } catch (Exception e) {
+            e.printStackTrace();
+    }
+  }
 
-	/**
-	 * Instantiates a new heal sphere.
-	 * 
-	 * @param ind the ind
-	 */
-	public HealSphere(HealpixBase ind) {
-		if (ind != null) {
-			this.index = ind;
-		} else {
-			try {
-				index = new HealpixBase(nside,Scheme.NESTED);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		this.nside = ind.getNside();
-		if (nside >= 8)
-			step = 11;
-		if (nside >= 16)
-			step = 7;
-		if (nside >= 32)
-			step = 3;
-		if (nside >= 64)
-			step = 1;
-	}
+  /**
+    * Instantiates a new heal sphere.
+    *
+    * @param ind the ind
+    */
+  public HealSphere(HealpixBase ind) {
+    if (ind != null) {
+            this.index = ind;
+    } else {
+            try {
+                    index = new HealpixBase(nside,Scheme.NESTED);
+            } catch (Exception e) {
+                    e.printStackTrace();
+            }
+    }
+    this.nside = ind.getNside();
+    setStep();
+  }
 
-	/**
-	 * Fetch the geometical objects describing the sphere, or whatever part of i
-	 * this object represents.
-	 * 
-	 * @return the lines or polygons or whatever represent the geometry of this
-	 *         object.
-	 */
-	protected abstract Geometry createGeometry();
+  /**
+    * Fetch the geometical objects describing the sphere, or whatever part of i
+    * this object represents.
+    *
+    * @return the lines or polygons or whatever represent the geometry of this
+    *         object.
+    */
+  protected abstract Geometry createGeometry();
 
-	/**
-	 * Add lines corresponding to the outline of the pixel defined by the set of
-	 * points.
-	 * 
-	 * @param points
-	 *            the vertices
-	 * @param offset
-	 *            the position in quads at which to start writing values.
-	 * @param quads
-	 *            structure into which to insert lines.
-	 */
-	protected void addPix(Vec3[] points, int offset, LineArray quads) {
-		int point = 0;
-		for (int c = 0; c < points.length; c++) {
-			// System.out.println("point:"+(offset+point)+" "+corners[c]);
-			quads.setCoordinate((offset + point++), new Point3d(points[c].x,
-					points[c].y, points[c].z));
-			if (c > 0) { // add it again
-				quads.setCoordinate((offset + point++), new Point3d(points[c]
-						.x, points[c].y, points[c].z));
-			}
-		}
-		quads.setCoordinate((offset + point++), new Point3d(points[0].x,
-				points[0].y, points[0].z));
-	}
+  /**
+    * Add lines corresponding to the outline of the pixel defined by the set of
+    * points.
+    *
+    * @param points
+    *            the vertices
+    * @param offset
+    *            the position in quads at which to start writing values.
+    * @param quads
+    *            structure into which to insert lines.
+    */
+  protected void addPix(Vec3[] points, int offset, LineArray quads) {
+    int point = 0;
+    for (int c = 0; c < points.length; c++) {
+      // System.out.println("point:"+(offset+point)+" "+corners[c]);
+      quads.setCoordinate((offset + point++), new Point3d(points[c].x,
+                      points[c].y, points[c].z));
+      if (c > 0) { // add it again
+              quads.setCoordinate((offset + point++), new Point3d(points[c]
+                              .x, points[c].y, points[c].z));
+      }
+    }
+    quads.setCoordinate((offset + point++), new Point3d(points[0].x,
+                    points[0].y, points[0].z));
+  }
 
-	/**
-	 * Convenience method for creating Appearance object.
-	 * 
-	 * @return appearance object with color.
-	 */
-	protected Appearance createAppearance() {
+  /**
+    * Convenience method for creating Appearance object.
+    *
+    * @return appearance object with color.
+    */
+  protected Appearance createAppearance() {
 
-		Appearance app = new Appearance();
-		ColoringAttributes ca = new ColoringAttributes(new Color3f(
-				java.awt.Color.GREEN), ColoringAttributes.SHADE_FLAT);
-		app.setColoringAttributes(ca);
+          Appearance app = new Appearance();
+          ColoringAttributes ca = new ColoringAttributes(new Color3f(
+                          java.awt.Color.GREEN), ColoringAttributes.SHADE_FLAT);
+          app.setColoringAttributes(ca);
 
-		return app;
-	}
-}
+          return app;
+  }
+  }
