32ef5174beb2bc9a9a45028b849f5d05904dc2b3
reinecke <reinecke@5e3cd54f-fed5-4059-89be-b68eacfec290>
2011-11-30 17:10:51 +0000

    - remove libpsht test codes (not needed in this package) - include alice test into standard tests - remove alice test data (no longer used)
    git-svn-id: svn://svn.code.sf.net/p/healpix/code/trunk@197 5e3cd54f-fed5-4059-89be-b68eacfec290

--- src/cxx/libpsht/planck.make
+++ src/cxx/libpsht/planck.make
@@ -7,9 +7,8 @@ FULL_INCLUDE+= -I$(SD)
 
 HDR_$(PKG):=$(SD)/*.h
 LIB_$(PKG):=$(LIBDIR)/libpsht.a
-BIN:=psht_test psht_perftest psht_test_mpi
 LIBOBJ:=ylmgen_c.o psht.o psht_geomhelpers.o psht_almhelpers.o
-ALLOBJ:=$(LIBOBJ) psht_test.o psht_perftest.o psht_test_mpi.o
+ALLOBJ:=$(LIBOBJ)
 LIBOBJ:=$(LIBOBJ:%=$(OD)/%)
 ALLOBJ:=$(ALLOBJ:%=$(OD)/%)
 
@@ -19,9 +18,6 @@ BDEP:=$(LIB_$(PKG)) $(LIB_libfftpack) $(LIB_c_utils)
 $(LIB_$(PKG)): $(LIBOBJ)
 
 $(ALLOBJ): $(ODEP) | $(OD)_mkdir
-BIN:=$(BIN:%=$(BINDIR)/%)
-$(BIN): $(BINDIR)/% : $(OD)/%.o $(BDEP)
 
 all_hdr+=$(HDR_$(PKG))
 all_lib+=$(LIB_$(PKG))
-all_cbin+=$(BIN)
--- src/cxx/test/runtest.sh
+++ src/cxx/test/runtest.sh
@@ -15,4 +15,6 @@ time $BINPATH/alm2map_cxx nlmax=512 infile=test.alm outfile=!test4.fits nside=31
 time $BINPATH/map2tga test4.fits test5.tga -bar -title "Synthesized Map (Nside=317)" && \
 time $BINPATH/median_filter_cxx test.fits '!test5.fits' 60 && \
 time $BINPATH/map2tga test5.fits test6.tga -bar -title "Median-filtered map (1 degree)" && \
-xv test.tga test2.tga test3.tga test4.tga test5.tga test6.tga
+time $BINPATH/smoothing_cxx nlmax=512 infile=test.fits outfile=\!test7.fits polarisation=true fwhm_arcmin=300 && \
+time $BINPATH/alice2 -in test7.fits -nside 256 -ell 100 -xsz 1024 -out test_alice && \
+xv test.tga test2.tga test3.tga test4.tga test5.tga test6.tga test_alice*.tga
