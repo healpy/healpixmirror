243bdb4f2b1672d093e05c231ab622ef1fa1cc81
reinecke <reinecke@5e3cd54f-fed5-4059-89be-b68eacfec290>
2012-05-21 10:44:56 +0000

    - separate unit tests and performance tests - tweak classpath
    git-svn-id: svn://svn.code.sf.net/p/healpix/code/trunk@290 5e3cd54f-fed5-4059-89be-b68eacfec290

---
+++ src/java/src/healpix/newcore/test/FastMathPerfTest.java
@@ -0,0 +1,104 @@
+package healpix.newcore.test;
+
+import junit.framework.TestCase;
+import java.text.DecimalFormat;
+import static healpix.newcore.FastMath.*;
+
+/** A class to perform correctness and speed tests for the FastMath class
+ *
+ * @author Naoki Shibata
+ */
+public class FastMathPerfTest extends TestCase
+  {
+  static private final DecimalFormat form = new DecimalFormat("##0.00");
+  final int ncomp=10000000;
+
+  public void test_perf_math()
+    {
+    System.out.println("Rough performance test of math functions");
+    long cnt, tstart;
+    double dummy=0, di, time;
+    di=1./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=0; i<1; i+=di)
+       { dummy+=Math.sqrt(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("sqrt: " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=4*Math.PI/ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-2*Math.PI; i<2*Math.PI; i+=di)
+      { dummy+=Math.sin(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("sin : " + form.format(cnt/time*1e-6) + "MOps/s");
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-2*Math.PI; i<2*Math.PI; i+=di)
+      { dummy+=Math.cos(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("cos : " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=2./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-1; i<1; i+=di)
+      { dummy+=Math.acos(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("acos: " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=1000./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-500; i<500; i+=di)
+      { dummy+=Math.atan(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("atan: " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=1000./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-500; i<500; i+=di)
+      { dummy+=Math.atan2(i,2.9); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("atan2: " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=1000./ncomp;
+    }
+  public void test_perf_fastmath()
+    {
+    System.out.println("Rough performance test of fast math functions");
+    long cnt,tstart;
+    double dummy=0, di, time;
+    di=4*Math.PI/ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-2*Math.PI; i<2*Math.PI; i+=di)
+      { dummy+=sin(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("sin : " + form.format(cnt/time*1e-6) + "MOps/s");
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-2*Math.PI; i<2*Math.PI; i+=di)
+      { dummy+=cos(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("cos : " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=2./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-1; i<1; i+=di)
+      { dummy+=acos(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("acos: " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=1000./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-500; i<500; i+=di)
+      { dummy+=atan(i); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("atan: " + form.format(cnt/time*1e-6) + "MOps/s");
+    di=1000./ncomp;
+    tstart = System.nanoTime();
+    cnt=0;
+    for (double i=-500; i<500; i+=di)
+      { dummy+=atan2(i,2.9); ++cnt; }
+    time = 1e-9*(System.nanoTime()-tstart);
+    System.out.println("atan2: " + form.format(cnt/time*1e-6) + "MOps/s");
+    }
+  }
