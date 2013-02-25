dc9924925ba712f63a85e85949629deb3968579f
hivon <hivon@5e3cd54f-fed5-4059-89be-b68eacfec290>
2013-02-25 10:18:31 +0000

    add images
    git-svn-id: svn://svn.code.sf.net/p/healpix/code/trunk@438 5e3cd54f-fed5-4059-89be-b68eacfec290

--- doc/www/htdocs/gallery.php
+++ doc/www/htdocs/gallery.php
@@ -5,10 +5,13 @@
 
 <h2>Screenshots</h2>
 
-<p>
-Coming soon
-</p>
+<p>Coming soon</p>
 
+<!--
+<p>
+<img src="images/wmapV_9yr" alt="WMAP 9yr Example" width="513" height="510"><br />
+</p>
+-->
 
 
 <?php require('_footer.inc.php'); ?>
