7cfcfd39ef585c137584c9d20dfcba680c5a4a92
hivon <hivon@5e3cd54f-fed5-4059-89be-b68eacfec290>
2013-04-03 16:25:16 +0000

    git-svn-id: svn://svn.code.sf.net/p/healpix/code/trunk@480 5e3cd54f-fed5-4059-89be-b68eacfec290

--- doc/www/htdocs/_analytics_tracking.php
+++ doc/www/htdocs/_analytics_tracking.php
@@ -1,13 +1,14 @@
-<script>
+<script type="text/javascript">
   (function(i,s,o,g,r,a,m){
   i['GoogleAnalyticsObject']=r;
   i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();
   a=s.createElement(o),m=s.getElementsByTagName(o)[0];
   a.async=1;
   a.src=g;
-  m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  m.parentNode.insertBefore(a,m);
+  })(window,document,'script','//www.google-analytics.com/analytics.js','_ga');
 
-  ga('create', 'UA-39562884-1', 'sourceforge.net');
-  ga('send', 'pageview');
+  _ga('create', 'UA-39562884-1', 'healpix.sourceforge.net');
+  _ga('send', 'pageview');
 
 </script>
