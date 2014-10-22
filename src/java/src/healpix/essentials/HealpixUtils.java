25a1f043d1846ef3e8f4cb19e5891cc1138a08a1
reinecke <reinecke@5e3cd54f-fed5-4059-89be-b68eacfec290>
2014-10-22 09:32:25 +0000

    switch to fastest Peano code
    git-svn-id: svn://svn.code.sf.net/p/healpix/code/trunk@626 5e3cd54f-fed5-4059-89be-b68eacfec290

--- src/java/src/healpix/essentials/HealpixUtils.java
+++ src/java/src/healpix/essentials/HealpixUtils.java
@@ -86,118 +86,25 @@ public class HealpixUtils
   static public int uniq2order (long uniq)
     { return ilog2(uniq>>>2)>>>1; }
 
-static public void tableprinter()
-  {
-    final byte peano_subpix[][][] =
-      { { {0,1,3,2}, {3,0,2,1}, {2,3,1,0}, {1,2,0,3},
-          {0,3,1,2}, {1,0,2,3}, {2,1,3,0}, {3,2,0,1} },
-        { {0,1,3,2}, {1,3,2,0}, {3,2,0,1}, {2,0,1,3},
-          {0,2,3,1}, {1,0,2,3}, {3,1,0,2}, {2,3,1,0} } };
-    final byte peano_subpath[][][] =
-      { { {4,0,6,0}, {7,5,1,1}, {2,4,2,6}, {3,3,7,5},
-          {0,2,4,4}, {5,1,5,3}, {6,6,0,2}, {1,7,3,7} },
-        { {4,0,0,6}, {5,1,1,7}, {6,2,2,4}, {7,3,3,5},
-          {0,4,4,2}, {1,5,5,3}, {2,6,6,0}, {3,7,7,1} } };
-  for (int dir=0; dir<2; ++dir)
-    {
-String res=new String();
-    for (int path=0; path<8; ++path)
-      {
-      for (int pix=0; pix<16; ++pix)
-        {
-        int pix2=peano_subpix[dir][path][pix>>>2],
-            path2=peano_subpath[dir][path][pix>>>2];
-        int val=(pix2<<2) | peano_subpix[dir][path2][pix&0x3]
-               +(peano_subpath[dir][path2][pix&0x3]<<4)
-               +(dir<<7);
-        res+=val+",";
-        }
-res+="\n";
-      }
-    System.out.println(res);
-    }
-  }
-
-  // classic version, two 3D lookup tables in the loop
   static private long nest_peano_helper (long pix, int order, int dir)
     {
-    final byte peano_subpix[][][] =
-      { { {0,1,3,2}, {3,0,2,1}, {2,3,1,0}, {1,2,0,3},
-          {0,3,1,2}, {1,0,2,3}, {2,1,3,0}, {3,2,0,1} },
-        { {0,1,3,2}, {1,3,2,0}, {3,2,0,1}, {2,0,1,3},
-          {0,2,3,1}, {1,0,2,3}, {3,1,0,2}, {2,3,1,0} } };
-    final byte peano_subpath[][][] =
-      { { {4,0,6,0}, {7,5,1,1}, {2,4,2,6}, {3,3,7,5},
-          {0,2,4,4}, {5,1,5,3}, {6,6,0,2}, {1,7,3,7} },
-        { {4,0,0,6}, {5,1,1,7}, {6,2,2,4}, {7,3,3,5},
-          {0,4,4,2}, {1,5,5,3}, {2,6,6,0}, {3,7,7,1} } };
-    final byte peano_face2path[][] =
-      { { 2,5,2,5,3,6,3,6,2,3,2,3 }, { 2,6,2,3,3,5,2,6,2,3,3,5 } };
-    final byte peano_face2face[][] =
-      { { 0,5,6,11,10,1,4,7,2,3,8,9 }, { 0,5,8,9,6,1,2,7,10,11,4,3 } };
-
-    int face = (int)(pix>>>(2*order));
-    long result = 0L;
-    byte path = peano_face2path[dir][face];
-
-    for (int shift=2*order-2; shift>=0; shift-=2)
-      {
-      byte spix = (byte)((pix>>shift) & 0x3);
-      result <<= 2;
-      result |= peano_subpix[dir][path][spix];
-      path=peano_subpath[dir][path][spix];
-      }
-
-    return result + (((long)peano_face2face[dir][face])<<(2*order));
-    }
-
-  // fairly standard version, one 1D lookup table in the loop
-  static private long nest_peano_helper2 (long pix, int order, int dir)
-    {
-    final byte arr[] = { 16, 1,27, 2,31,20, 6, 5,10,19, 9,24,13,14,28,23,
-                          0,11,17,18,21, 4,22,15,26,25, 3, 8, 7,30,12,29,
-                         48,33,35,58,53,39,38,60,59,42,40,49,62,44,45,55,
-                         32,50,51,41,37,52,54,47,43,57,56,34,46,63,61,36 };
-
-    final byte peano_face2path[][] =
-      { { 2,5,2,5,3,6,3,6,2,3,2,3 }, { 2,6,2,3,3,5,2,6,2,3,3,5 } };
-    final byte peano_face2face[][] =
-      { { 0,5,6,11,10,1,4,7,2,3,8,9 }, { 0,5,8,9,6,1,2,7,10,11,4,3 } };
-
-    int face = (int)(pix>>>(2*order));
-    long result = 0L;
-    int state = (peano_face2path[dir][face]<<2)|(dir<<5);
-
-    for (int shift=2*order-2; shift>=0; shift-=2)
-      {
-      state=arr[(state&0xFC) | (((int)(pix>>>shift))&0x3)];
-      result = (result<<2) | (state&0x3);
-      }
-
-    return result + (((long)peano_face2face[dir][face])<<(2*order));
-    }
-  // 1D lookup table in the loop, do two steps at once as long as possible
-  static private long nest_peano_helper3 (long pix, int order, int dir)
-    {
-    final short arr2[] = {
-        0, 35, 65, 66, 68,  5,103,  6,110,109, 15, 44, 72,  9,107, 10,
-       31,126, 60,125, 81, 16, 82, 51,123, 88, 26, 25,119, 84, 22, 21,
-       42, 75, 41,104, 12, 47, 77, 78, 38, 71, 37,100, 98, 97,  3, 32,
-       53, 54,116, 87, 57, 58,120, 91, 19,114, 48,113, 93, 28, 94, 63,
-       64,  1, 99,  2, 46, 79, 45,108,  4, 39, 69, 70,  8, 43, 73, 74,
-       85, 20, 86, 55,115, 80, 18, 17, 89, 24, 90, 59, 61, 62,124, 95,
-      106,105, 11, 40,102,101,  7, 36, 76, 13,111, 14, 34, 67, 33, 96,
-      127, 92, 30, 29, 27,122, 56,121, 49, 50,112, 83, 23,118, 52,117,
-
-      128,194,195,161,196,133,135,230,204,141,143,238,171,233,232,138,
-      149,212,214,183,221,159,158,252,217,155,154,248,178,243,241,144,
-      175,237,236,142,235,170,168,201,227,162,160,193,132,198,199,165,
-      186,251,249,152,242,176,177,211,246,180,181,215,157,220,222,191,
-      192,129,131,226,136,202,203,169,140,206,207,173,231,166,164,197,
-      213,151,150,244,145,208,210,179,153,216,218,187,254,188,189,223,
-      239,174,172,205,167,229,228,134,163,225,224,130,200,137,139,234,
-      250,184,185,219,190,255,253,156,182,247,245,148,209,147,146,240
-      };
+    final short arr2[] =
+      {   0, 35, 65, 66, 68,  5,103,  6,110,109, 15, 44, 72,  9,107, 10,
+         31,126, 60,125, 81, 16, 82, 51,123, 88, 26, 25,119, 84, 22, 21,
+         42, 75, 41,104, 12, 47, 77, 78, 38, 71, 37,100, 98, 97,  3, 32,
+         53, 54,116, 87, 57, 58,120, 91, 19,114, 48,113, 93, 28, 94, 63,
+         64,  1, 99,  2, 46, 79, 45,108,  4, 39, 69, 70,  8, 43, 73, 74,
+         85, 20, 86, 55,115, 80, 18, 17, 89, 24, 90, 59, 61, 62,124, 95,
+        106,105, 11, 40,102,101,  7, 36, 76, 13,111, 14, 34, 67, 33, 96,
+        127, 92, 30, 29, 27,122, 56,121, 49, 50,112, 83, 23,118, 52,117,
+        128,194,195,161,196,133,135,230,204,141,143,238,171,233,232,138,
+        149,212,214,183,221,159,158,252,217,155,154,248,178,243,241,144,
+        175,237,236,142,235,170,168,201,227,162,160,193,132,198,199,165,
+        186,251,249,152,242,176,177,211,246,180,181,215,157,220,222,191,
+        192,129,131,226,136,202,203,169,140,206,207,173,231,166,164,197,
+        213,151,150,244,145,208,210,179,153,216,218,187,254,188,189,223,
+        239,174,172,205,167,229,228,134,163,225,224,130,200,137,139,234,
+        250,184,185,219,190,255,253,156,182,247,245,148,209,147,146,240 };
     final byte arr[] = { 16, 1,27, 2,31,20, 6, 5,10,19, 9,24,13,14,28,23,
                           0,11,17,18,21, 4,22,15,26,25, 3, 8, 7,30,12,29,
                          48,33,35,58,53,39,38,60,59,42,40,49,62,44,45,55,
@@ -224,48 +131,10 @@ res+="\n";
 
     return result + (((long)peano_face2face[dir][face])<<(2*order));
     }
-  // no lookup tables in the loop, use long integers instead.
-  // This version could be vectorized, otherwise it's probably worthless
-  static private long nest_peano_helper4 (long pix, int order, int dir)
-    {
-    final byte peano_face2path[][] =
-      { { 2,5,2,5,3,6,3,6,2,3,2,3 }, { 2,6,2,3,3,5,2,6,2,3,3,5 } };
-    final byte peano_face2face[][] =
-      { { 0,5,6,11,10,1,4,7,2,3,8,9 }, { 0,5,8,9,6,1,2,7,10,11,4,3 } };
-
-    int face = (int)(pix>>>(2*order));
-    long result = 0L;
-    int state = (peano_face2path[dir][face]<<2);
-    if (dir==1)
-      {
-      final long a=2199974852034244020L, b=9163371139704739200L,
-                 c=1446803457519468865L;
-      for (int shift=2*order-2; shift>=0; shift-=2)
-        {
-        state=(state&0xFC)|((int)(pix>>>shift) & 0x3);
-        state=(state&0x3)|((int)((b>>>(2*state))&0x3)<<2)
-                         |((int)((c>>>(2*state))&0x3)<<4);
-        result = (result<<2) | (a>>>(2*state))&0x3;
-        }
-      }
-    else
-      {
-      final long a=5419767265092330420L, b=-177095002849913056L,
-                 c=4901342806104343825L;
-      for (int shift=2*order-2; shift>=0; shift-=2)
-        {
-        state=(state&0xFC)|((int)(pix>>>shift) & 0x3);
-        state=(state&0x3)|((int)((b>>>(2*state))&0x3)<<2)
-                         |((int)((c>>>(2*state))&0x3)<<4);
-        result = (result<<2) | (a>>>(2*state))&0x3;
-        }
-      }
-    return result + (((long)peano_face2face[dir][face])<<(2*order));
-    }
 
   static public long nest2peano(long pix, int order)
-    { return nest_peano_helper3(pix,order,0); }
+    { return nest_peano_helper(pix,order,0); }
   static public long peano2nest(long pix, int order)
-    { return nest_peano_helper3(pix,order,1); }
+    { return nest_peano_helper(pix,order,1); }
 
   }
