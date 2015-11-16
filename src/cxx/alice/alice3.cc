dcc859ac84a10da0b4abc81653cb99a0f2320874
reinecke <reinecke@5e3cd54f-fed5-4059-89be-b68eacfec290>
2015-11-16 09:52:26 +0000

    remove C++11 features for now
    git-svn-id: svn://svn.code.sf.net/p/healpix/code/branches/branch_v330r807@809 5e3cd54f-fed5-4059-89be-b68eacfec290

--- src/cxx/alice/alice3.cc
+++ src/cxx/alice/alice3.cc
@@ -236,7 +236,11 @@ int main(int argc, const char** argv)
 
       a(ell,0)=fcomplex(rng.rand_gauss(),0.);
       for (int m=0; m<=ell; m++)
-        { a(ell,m).real(rng.rand_gauss()); a(ell,m).imag(rng.rand_gauss()); }
+        {
+        double t1,t2;
+        t1=rng.rand_gauss(); t2=rng.rand_gauss();
+        a(ell,m)=xcomplex<float>(t1,t2);
+        }
       alm2map(a, th);
       }
     else
